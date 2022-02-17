59d420a9a84a472e9e4d52676a0f303205d5cc58
shorlak <junco466@gmail.com>
2022-02-16 21:30:00 -0500

    copiado, y preparado para deploy

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import Template from '@templates/Template.js';
+import '@styles/main.css';
+import '@styles/vars.styl'
+
+//prueba para ver el cambio del watch
+console.log("hola")
+
+(async function App() {
+  const main = null || document.getElementById('main');
+  main.innerHTML = await Template();
+})();
---
+++ src/utils/getData.js
@@ -0,0 +1,16 @@
+//Aqui hago el llamado de la api desde una variable de entrorno
+//configurada con webpaxk, y que se encuentra en el archivo .env
+const API = process.env.API;
+
+const getData = async (id) => {
+  const apiURl = id ? `${API}${id}` : API;
+  try {
+    const response = await fetch(apiURl);
+    const data = await response.json();
+    return data.results[0];
+  } catch (error) {
+    console.log('Fetch Error', error);
+  };
+};
+
+export default getData;
